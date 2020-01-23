47b5c23d123d207ce3280150508db06ea8472b0c
Giano <francesco.giannu@gmail.com>
2020-01-23 16:17:14 +0100

    Implementazione nuovi materiali
    -Aggiunto materiale sprovvisto di envMap
    -Aggiunto materiale GOLDEN

--- libs/shaders.js
+++ libs/shaders.js
@@ -157,9 +157,243 @@ void main() {
     (4.0 * nDotl2 * nDotv);
     
     
-    vec3 outRadiance = (PI * clight * nDotl * BRDF * envLight) + (PI * clight * nDotl1 * BRDF1 * envLight) + (PI * clight * nDotl2 * BRDF2 * envLight) + ambientLight * cdiff * texture2D(aoMap, uVv * textureRepeat).xyz;
+    vec3 outRadiance = ((PI * clight * nDotl * BRDF * envLight) + (PI * clight * nDotl1 * BRDF1 * envLight) + (PI * clight * nDotl2 * BRDF2 * envLight) + ambientLight )* cdiff * texture2D(aoMap, uVv * textureRepeat).xyz;
     // gamma encode the final value
     gl_FragColor = vec4(pow(outRadiance, vec3(1.0 / 2.2)), 1.0);
     }
 `
 }
+
+function fragmentShader1() {
+    return `
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying vec2 uVv;
+varying vec3 wPosition;
+uniform vec3 pointLightPosition; // in world space
+uniform vec3 pointLightPosition1; // in world space
+uniform vec3 pointLightPosition2; // in world space
+uniform vec3 clight;
+uniform vec3 ambientLight;
+uniform sampler2D specularMap;
+uniform sampler2D diffuseMap;
+uniform sampler2D roughnessMap;
+uniform sampler2D normalMap;
+uniform sampler2D aoMap;
+uniform vec2 normalScale;
+uniform vec2 textureRepeat;
+uniform samplerCube envMap;
+const float PI = 3.14159;
+
+vec3 cdiff;
+vec3 cspec;
+float roughness;
+
+struct SpotLight {
+    vec3 color;
+    vec3 position; // light position, in camera coordinates
+    float distance; 
+};
+   
+uniform SpotLight spotLights[NUM_SPOT_LIGHTS];
+
+
+vec3 FSchlick(float lDoth) {
+    return (cspec + (vec3(1.0) - cspec) * pow(1.0 - lDoth, 5.0));
+}
+
+float DGGX(float nDoth, float alpha) {
+    float alpha2 = alpha * alpha;
+    float d = nDoth * nDoth * (alpha2 - 1.0) + 1.0;
+    return (alpha2 / (PI * d * d));
+}
+
+float G1(float dotProduct, float k) {
+    return (dotProduct / (dotProduct * (1.0 - k) + k));
+}
+
+float GSmith(float nDotv, float nDotl) {
+    float k = roughness * roughness;
+    return G1(nDotl, k) * G1(nDotv, k);
+}
+
+vec3 perturbNormal2Arb(vec3 eye_pos, vec3 surf_norm) {
+    vec3 q0 = dFdx(eye_pos.xyz);
+    vec3 q1 = dFdy(eye_pos.xyz);
+    vec2 st0 = dFdx(uVv.st);
+    vec2 st1 = dFdy(uVv.st);
+    vec3 S = normalize(q0 * st1.t - q1 * st0.t);
+    vec3 T = normalize(-q0 * st1.s + q1 * st0.s);
+    vec3 N = surf_norm;
+    vec3 mapN = texture2D(normalMap, uVv * textureRepeat).xyz * 2.0 - 1.0;
+    mapN.xy = normalScale * mapN.xy;
+    mat3 tsn = mat3(S, T, N);
+    return normalize(tsn * mapN);
+}
+
+vec3 inverseTransformDirection( in vec3 dir, in mat4 matrix) {
+    return normalize((vec4(dir, 0.0) * matrix).xyz);
+}
+
+void main() {
+    
+    vec3 n = perturbNormal2Arb(vPosition, normalize(vNormal));
+    vec4 lPosition = vec4(spotLights[0].position, 1.0);
+    vec4 lPosition1 = vec4(spotLights[1].position, 1.0);
+    vec4 lPosition2 = vec4(spotLights[2].position, 1.0);
+    vec3 l = normalize(lPosition.xyz - vPosition.xyz);
+    vec3 l1 = normalize(lPosition1.xyz - vPosition.xyz);
+    vec3 l2 = normalize(lPosition2.xyz - vPosition.xyz);
+    vec3 v = normalize(-vPosition);
+    vec3 h = normalize(v + l);
+    vec3 h1 = normalize(v + l1);
+    vec3 h2 = normalize(v + l2);
+    
+    //vec3 n = normalize( vNormal );  // interpolation destroys normalization, so we have to normalize
+    
+
+    // small quantity to prevent divisions by 0
+    float nDotl = max(dot(n, l), 0.000001);
+    float lDoth = max(dot(l, h), 0.000001);
+    float nDoth = max(dot(n, h), 0.000001);
+    float vDoth = max(dot(v, h), 0.000001);
+    
+    float nDotv = max(dot(n, v), 0.000001);
+    
+    // small quantity to prevent divisions by 0
+    float nDotl1 = max(dot(n, l1), 0.000001);
+    float lDoth1 = max(dot(l1, h1), 0.000001);
+    float nDoth1 = max(dot(n, h1), 0.000001);
+    float vDoth1 = max(dot(v, h1), 0.000001);
+    
+    // small quantity to prevent divisions by 0
+    float nDotl2 = max(dot(n, l2), 0.000001);
+    float lDoth2 = max(dot(l2, h2), 0.000001);
+    float nDoth2 = max(dot(n, h2), 0.000001);
+    float vDoth2 = max(dot(v, h2), 0.000001);
+    
+    
+    cdiff = texture2D(diffuseMap, uVv * textureRepeat).rgb;
+    // texture in sRGB, linearize
+    cdiff = pow(cdiff, vec3(2.2));
+    cspec = texture2D(specularMap, uVv * textureRepeat).rgb;
+    // texture in sRGB, linearize
+    cspec = pow(cspec, vec3(2.2));
+    roughness = texture2D(roughnessMap, uVv * textureRepeat).r; // no need to linearize roughness map
+    
+    
+    vec3 worldN = inverseTransformDirection(n, viewMatrix);
+    vec3 worldV = cameraPosition - wPosition;
+    vec3 r = normalize(reflect(-worldV, worldN));
+    
+    vec3 fresnel = FSchlick(lDoth);
+    vec3 fresnel1 = FSchlick(lDoth1);
+    vec3 fresnel2 = FSchlick(lDoth2);
+    
+    vec3 BRDF = (vec3(1.0) - fresnel) * cdiff / PI + fresnel * GSmith(nDotv, nDotl) * DGGX(nDoth, roughness * roughness) /
+    (4.0 * nDotl * nDotv);
+    
+    vec3 BRDF1 = (vec3(1.0) - fresnel1) * cdiff / PI + fresnel1 * GSmith(nDotv, nDotl1) * DGGX(nDoth1, roughness * roughness) /
+    (4.0 * nDotl1 * nDotv);
+    
+    vec3 BRDF2 = (vec3(1.0) - fresnel2) * cdiff / PI + fresnel2 * GSmith(nDotv, nDotl2) * DGGX(nDoth2, roughness * roughness) /
+    (4.0 * nDotl2 * nDotv);
+    
+    
+    vec3 outRadiance = ((PI * clight * nDotl * BRDF) + (PI * clight * nDotl1 * BRDF1) + (PI * clight * nDotl2 * BRDF2) + ambientLight) * cdiff * texture2D(aoMap, uVv * textureRepeat).xyz;
+    // gamma encode the final value
+    gl_FragColor = vec4(pow(outRadiance, vec3(1.0 / 2.2)), 1.0);
+    }
+`
+}
+
+function fragmentShaderGOLDEN() {
+    return `
+			varying vec3 vNormal;
+			varying vec3 vPosition;
+			uniform vec3 pointLightPosition; // in world space
+			uniform vec3 pointLightPosition1; // in world space
+			uniform vec3 pointLightPosition2; // in world space
+			uniform vec3 pointLightPosition3; // in world space
+			uniform vec3 clight;
+			uniform vec3 cspec;
+			uniform float roughness;
+            const float PI = 3.14159;
+            
+			vec3 FSchlick(float lDoth) {
+				return (cspec + (vec3(1.0)-cspec)*pow(1.0 - lDoth,5.0));
+			}
+			float DGGX(float nDoth, float alpha) {
+				float alpha2 = alpha*alpha;
+				float d = nDoth*nDoth*(alpha2-1.0)+1.0;
+				return (  alpha2 / (PI*d*d));
+			}
+			float G1(float dotProduct, float k) {
+				return (dotProduct / (dotProduct*(1.0-k) + k) );
+			}
+			float GSmith(float nDotv, float nDotl) {
+					float k = roughness*roughness;
+					return G1(nDotl,k)*G1(nDotv,k);
+			}
+			void main() {
+				vec4 lPosition = viewMatrix * vec4( pointLightPosition, 1.0 );
+				vec4 lPosition1 = viewMatrix * vec4( pointLightPosition1, 1.0 );
+				vec4 lPosition2 = viewMatrix * vec4( pointLightPosition2, 1.0 );
+				vec4 lPosition3 = viewMatrix * vec4( pointLightPosition3, 1.0 );
+
+				vec3 l = normalize(lPosition.xyz - vPosition.xyz);
+				vec3 l1 = normalize(lPosition1.xyz - vPosition.xyz);
+				vec3 l2 = normalize(lPosition2.xyz - vPosition.xyz);
+				vec3 l3 = normalize(lPosition3.xyz - vPosition.xyz);
+				
+				vec3 n = normalize( vNormal );  // interpolation destroys normalization, so we have to normalize
+				vec3 v = normalize( -vPosition);
+
+				vec3 h = normalize( v + l);
+				vec3 h1 = normalize( v + l1);
+				vec3 h2 = normalize( v + l2);
+				vec3 h3 = normalize( v + l3);
+				
+				// small quantity to prevent divisions by 0
+				float nDotl = max(dot( n, l ),0.000001);
+				float nDotl1 = max(dot( n, l1 ),0.000001);
+				float nDotl2 = max(dot( n, l2 ),0.000001);
+				float nDotl3 = max(dot( n, l3 ),0.000001);
+
+				float lDoth = max(dot( l, h ),0.000001);
+				float lDoth1 = max(dot( l1, h1 ),0.000001);
+				float lDoth2 = max(dot( l2, h2 ),0.000001);
+				float lDoth3 = max(dot( l3, h3 ),0.000001);
+				
+				float nDoth = max(dot( n, h ),0.000001);
+				float nDoth1 = max(dot( n, h1 ),0.000001);
+				float nDoth2 = max(dot( n, h2 ),0.000001);
+				float nDoth3 = max(dot( n, h3 ),0.000001);
+				
+				float vDoth = max(dot( v, h ),0.000001);
+				float vDoth1 = max(dot( v, h1 ),0.000001);
+				float vDoth2 = max(dot( v, h2 ),0.000001);
+				float vDoth3 = max(dot( v, h3 ),0.000001);
+				
+				float nDotv = max(dot( n, v ),0.000001);
+				
+				vec3 specularBRDF = FSchlick(lDoth)*GSmith(nDotv,nDotl)*DGGX(nDoth,roughness*roughness)/
+					(4.0*nDotl*nDotv);
+				vec3 specularBRDF1 = FSchlick(lDoth1)*GSmith(nDotv,nDotl1)*DGGX(nDoth1,roughness*roughness)/
+					(4.0*nDotl1*nDotv);
+				vec3 specularBRDF2 = FSchlick(lDoth2)*GSmith(nDotv,nDotl2)*DGGX(nDoth2,roughness*roughness)/
+					(4.0*nDotl2*nDotv);
+				vec3 specularBRDF3 = FSchlick(lDoth3)*GSmith(nDotv,nDotl3)*DGGX(nDoth3,roughness*roughness)/
+					(4.0*nDotl3*nDotv); 
+				
+				vec3 outRadiance = (PI* clight * nDotl * specularBRDF) + 
+					(PI* clight * nDotl1 * specularBRDF1) +
+					(PI* clight * nDotl2 * specularBRDF2) +
+					(PI* clight * nDotl3 * specularBRDF3) ;
+					
+				// gamma encode the final value
+				gl_FragColor = vec4(pow( outRadiance, vec3(1.0/2.2)), 1.0);
+				//gl_FragColor = vec4(outRadiance,1.0);
+            }
+            `
+        }
